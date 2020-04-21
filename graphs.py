3e3e21ced93d5a575266c2b6ff88f2fd9383a399
Brandon Lindsey <bethany.lindsey@gBLc0a3a1.local>
2020-04-21 14:30:52 -0700

    Add twin x

--- graphs.py
+++ graphs.py
@@ -13,4 +13,24 @@ def shaded_alpha_area(p,n, alpha):
     ax.fill_between(x, normal_approx.pdf(x), 
                 where=(x >= normal_approx.ppf(1-alpha)), color="red", alpha=0.5)
     ax.set_title("Significance Region")
-    
+
+
+def twin_x():
+     #Twin X /  Different Y
+    fig, ax1 = plt.subplots(figsize=(8,5))
+
+    color = 'blue'
+    ax1.set_xlabel('')
+    ax1.set_ylabel(')  # we already handled the x-label with ax1
+    ax1.plot(x, y, color=color)
+    ax1.tick_params(axis='', labelcolor=color)
+    ax1.axhline(OD_mean)
+
+    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+
+    color = 'red'
+
+    ax2.set_ylabel('')
+    ax2.plot(x, y2, color=color)
+    ax2.tick_params(axis='', labelcolor=color)
