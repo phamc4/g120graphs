9d9c95ce42ae3151c6781095ca4c02c98d805aca
Brandon Lindsey <bethany.lindsey@gBLc0a3a1.local>
2020-04-21 14:06:28 -0700

    Add shaded Norm graph

--- graphs.py
+++ graphs.py
@@ -1,4 +1,16 @@
 #! python3
 
-def graph():
-    return 
+def shaded_alpha_area(p,n, alpha):
+    binomial_mean = p * n
+    binomial_var = n * p * (1-p)
+    normal_approx = stats.norm(binomial_mean, np.sqrt(binomial_var))
+
+    x = np.linspace(0,n,1000)
+
+    fig, ax = plt.subplots(1, figsize=(16, 3))
+
+    ax.plot(x, normal_approx.pdf(x), linewidth=3)
+    ax.fill_between(x, normal_approx.pdf(x), 
+                where=(x >= normal_approx.ppf(1-alpha)), color="red", alpha=0.5)
+    ax.set_title("Significance Region")
+    
